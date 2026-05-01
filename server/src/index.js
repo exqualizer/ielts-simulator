7e4f417da9336109371377fe85b4f710ba03738a
James Paredes <nicejames1818@gmail.com>
2026-05-01 14:36:44 +1000

    Allow methods and headers content

--- server/src/index.js
+++ server/src/index.js
@@ -43,6 +43,8 @@ app.use((req, res, next) => {
   if (origin && allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin)
     res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     res.setHeader('Vary', 'Origin')
   }
   next()
